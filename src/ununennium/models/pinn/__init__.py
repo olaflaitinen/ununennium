3a9670ea7e975cd4b69f11daefee782a4b955c3d
Gustav Olaf Yunus Laitinen-Fredriksson Lundström Imanov <olaf.laitinen@gmail.com>
2025-12-16 12:16:24 +0100

    feat: Introduce the PINN module and integrate its components into the models package.

--- src/ununennium/models/pinn/__init__.py
+++ src/ununennium/models/pinn/__init__.py
@@ -1,10 +1,6 @@
 """PINN (Physics-Informed Neural Networks) module."""
 
-from ununennium.models.pinn.base import (
-    MLP,
-    PDEEquation,
-    PINN,
-)
+from ununennium.models.pinn.base import MLP, PDEEquation, PINN
 
 from ununennium.models.pinn.collocation import (
     AdaptiveSampler,
